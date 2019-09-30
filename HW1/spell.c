b4e8e2f8ad211cf3368609fc56ec22af74340225
qiuey00 <qiuey00@gmail.com>
2019-09-29 21:06:23 -0400

    get to work

--- HW1/spell.c
+++ HW1/spell.c
@@ -12,14 +12,14 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
 
     FILE* fp = fopen(dictionary_file, "r");
 
-	if (!fp) {
-		printf("no file");
-		return false;
-	}
+    if (!fp) {
+      printf("no file");
+      return false;
+    }
 
     char str[LENGTH];
 
-	while(fgets(str, LENGTH, fp)) {
+    while(fgets(str, LENGTH, fp)) {
         for(int i = 0; i <= strlen(str); i++) {
             str[i] = tolower(str[i]);
         }
@@ -28,84 +28,109 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
             str[strlen(str)-1] = '\0';
         }
 
-    	int index = hash_function(str);
+        int index = hash_function(str);
 
-    	if (hashtable[index] == NULL) {
+        if (hashtable[index] == NULL) {
             node *aNode = malloc(sizeof(node));
             strcpy(aNode->word, str);
-	    	aNode->next = NULL;
-    		hashtable[index] = aNode;
-    	}
-    	else{
+            aNode->next = NULL;
+            hashtable[index] = aNode;
+        }
+        else{
             node *aNode = malloc(sizeof(node));
             strcpy(aNode->word, str);
-	    	aNode->next = hashtable[index];
-    		hashtable[index] = aNode;
-    	}
+            aNode->next = hashtable[index];
+            hashtable[index] = aNode;
+        }
     }
     fclose(fp);
     return true;
-
 }
 
 bool check_word(const char* word, hashmap_t hashtable[]) {
     char theWord[LENGTH];
-
     for(int i=0; i<=strlen(word); i++) {
         theWord[i] = tolower(word[i]);
     }
     int index = hash_function(theWord);
-	node* cursor = hashtable[index];
+    node* cursor = hashtable[index];
 
-	while(cursor) {
-		if (strcmp(cursor->word, theWord) == 0) { 
-			return true;
-		}
-		else{
-			cursor = cursor->next;
-		}
-	}
-	return false;
+    while(cursor) {
+      if (strcmp(cursor->word, theWord) == 0) { 
+         return true;
+     }
+     else{
+         cursor = cursor->next;
+     }
+ }
+ return false;
 }
 
 int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[MAX_MISSPELLED]) { 
-	int num_misspelled = 0;
+    char *pos = 0;
+    char *aWord;
+    int num_misspelled = 0;
+    char str[80];
 
-    char str[255];
-    char* token = strtok(str," ");
+    if (fp == NULL){
+        return false;
+    }
 
-    while (fgets(str, 255, fp)) {
-        token = strtok(NULL," ");
-    	while (token != NULL){
-            if (token[strlen(token)-1] =='\n'){
-            	token[strlen(token)-1]='\0';
+    while ( fgets(str, 80, fp) != NULL) {
+        if ((pos = strchr(str, '\n')) != NULL)
+            *pos = '\0';
+
+        aWord = strtok(str," ");
+
+        if (aWord != NULL && strlen(aWord) > LENGTH)
+            aWord = NULL;
+
+        char *temp = aWord + strlen(aWord) - 1;
+        char *temp2 = aWord;
+        while (ispunct(*temp)) {
+            temp++;
+        }
+        while (ispunct(*temp2) && temp < temp2) { 
+            *temp2 = 0; temp2--; 
+        }
+        aWord=temp;
+
+        while (aWord) {
+            if (check_word(aWord, hashtable)) {
+                aWord = strtok(NULL, " ");
+                char *temp = aWord+ strlen(aWord) - 1;
+                char *temp2 = aWord;
+                while (ispunct(*temp)) {
+                    temp++;
+                }
+                while (ispunct(*temp2) && temp < temp2) { 
+                    *temp2 = 0; temp2--; 
+                }
+                aWord=temp;
             }
-            token[strlen(token)]='\0';    
-
-            if (ispunct(token[strlen(token)-1])){
-                while(ispunct(token[strlen(token)-1])){
-                    token[strlen(token)-1]='\0';
+            else {
+                if (num_misspelled < MAX_MISSPELLED){
+                    misspelled[num_misspelled] = (char *) malloc((LENGTH+1)*sizeof(char));
+                    strncpy(misspelled[num_misspelled],aWord,(LENGTH+1));
+                    num_misspelled++;
+                    aWord = strtok(NULL, " ");
+                    if (aWord != NULL){
+                        char *temp = aWord+ strlen(aWord) - 1;
+                        char *temp2 = aWord;
+                        while (ispunct(*temp)) {
+                            temp++;
+                        }
+                        while (ispunct(*temp2) && temp < temp2) { 
+                            *temp2 = 0; temp2--; 
+                        }
+                        aWord=temp;
+                    }
+                }
+                else{
+                    return num_misspelled;
                 }
             }
-            if (ispunct(token[0])){
-                while(ispunct(token[0])){
-                    token++;                    
-                }
-            }
-
-			bool spellcheck = check_word(token,hashtable);
-            // if(!(spellcheck == 1 && strlen(str) < 255)){
-			if(!spellcheck){
-        		misspelled[num_misspelled] = malloc(strlen(token));
-            	strcpy(misspelled[num_misspelled], token);
-            	num_misspelled ++;
-                if (num_misspelled > MAX_MISSPELLED){
-                    break;
-                }
-            }
-            token = strtok(NULL," ");
-		}
-
+        }
     }
     return num_misspelled;
 }
